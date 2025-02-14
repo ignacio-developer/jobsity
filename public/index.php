7ac3d87be256a482022e6d71bb16df5160d97e4b
Ignacio Giampaoli <colo92@gmail.com>
2025-02-14 18:14:58 -0300

    Improving explainatory comments

--- public/index.php
+++ public/index.php
@@ -1,14 +1,21 @@
 <?php
 require_once '../app/Controllers/EventController.php';
 
-// Define available routes and their corresponding controllers/methods
+/*
+* E.g.: http://localhost:8000/index.php?action=viewEvent&id=df5d68f3-a21c-4f2a-bcd6-21320fb58f87
+* action = 'viewEvent'
+* id = 'df5d68f3-a21c-4f2a-bcd6-21320fb58f87'
+* will match EventController at line 23.
+*/
+
+// Define routes and their actions/methods
 $routes = [
     'listEvents' => ['controller' => 'EventController', 'method' => 'listEvents'],
     'viewEvent' => ['controller' => 'EventController', 'method' => 'viewEventDetails'],
     'addEvent' => ['controller' => 'EventController', 'method' => 'addEvent'],
 ];
 
-// Get the requested action, default to 'listEvents'
+// Get the requested action, if empty go to index page and 'listEvents'.
 $action = $_GET['action'] ?? 'listEvents';
 
 // Check if the requested action exists in the routes
@@ -19,7 +26,7 @@ if (isset($routes[$action])) {
     // Dynamically instantiate the correct controller
     $controller = new $controllerName();
 
-    // Remove `action` from $_GET, leaving only parameters
+    // Remove 'action' from $_GET, leaving only the parameters
     unset($_GET['action']);
     $params = array_values($_GET); // Get remaining query parameters as indexed array
 
