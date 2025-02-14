56162617a4621397f3b42840228274d6392dcc0b
Ignacio Giampaoli <colo92@gmail.com>
2025-02-14 17:51:10 -0300

    Improving routing index for dinamically load controllers and actions/methods

--- public/index.php
+++ public/index.php
@@ -1,15 +1,31 @@
 <?php
 require_once '../app/Controllers/EventController.php';
 
-$controller = new EventController();
+// Define available routes and their corresponding controllers/methods
+$routes = [
+    'listEvents' => ['controller' => 'EventController', 'method' => 'listEvents'],
+    'viewEvent' => ['controller' => 'EventController', 'method' => 'viewEventDetails'],
+    'addEvent' => ['controller' => 'EventController', 'method' => 'addEvent'],
+];
 
-// Simple routing based on `action` GET parameter
+// Get the requested action, default to 'listEvents'
 $action = $_GET['action'] ?? 'listEvents';
 
-if ($action === 'viewEvent' && isset($_GET['id'])) {
-    $controller->viewEventDetails($_GET['id']);
-} elseif ($action === 'addEvent') {
-    $controller->addEvent();
-}  else {
-    $controller->listEvents();
+// Check if the requested action exists in the routes
+if (isset($routes[$action])) {
+    $controllerName = $routes[$action]['controller'];
+    $method = $routes[$action]['method'];
+
+    // Dynamically instantiate the correct controller
+    $controller = new $controllerName();
+
+    // Remove `action` from $_GET, leaving only parameters
+    unset($_GET['action']);
+    $params = array_values($_GET); // Get remaining query parameters as indexed array
+
+    // Call the controller method with dynamic parameters
+    call_user_func_array([$controller, $method], $params);
+} else {
+    http_response_code(404);
+    echo "404 - Page Not Found";
 }
